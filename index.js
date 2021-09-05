270ad85a01b5210e5e19da46f260ff74af736b9f
Jignesh <jigneshmailbag@gmail.com>
2021-09-04 21:02:07 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b){
+    return a+b;
+}
+add(a, b);
+function subtract(a, b){
+    return a-b;
+}
+subtract(a, b);
+function multiply(a, b){
+    return a*b;
+}
+multiply(a, b);
+function divide(a, b){
+    return a/b;
+}
+divide(a, b);
+function increment(n){
+    return n+1
+}
+increment(n);
+function decrement(n){
+    return n-1
+}
+decrement(n);
+function makeInt(n){
+    return parseInt(n, 10);
+}
+makeInt(n);
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
+preserveDecimal(n);
